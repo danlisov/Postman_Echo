495ed04b9d981710349cb6ed847b7ce08f5e6ff5
Denis Lysov <dan.lysov2015@yandex.ru>
2023-09-23 14:37:18 +0300

    Initial commit

---
+++ src/test/java/ru/netology/PostamEchoTest.java
@@ -0,0 +1,25 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostamEchoTest {
+    @Test
+    void shouldReturnBodyPostman() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hello Postman Echo") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Hello Postman"))
+        ;
+    }
+}
